62c5ccb5269a8189251ae6b87c8c05ad61ab2760
botTaco <31516380+botTaco@users.noreply.github.com>
2018-04-11 13:34:43 -0500

    Add files via upload

---
+++ control.cpp
@@ -0,0 +1,4 @@
+//Este programa es un dejo de compromiso y quien lo tome en serio es un lastre social
+//Model constrains: direction axis posible rotation, should be between ()-pi/2,pi/2) <---note: doesnot includes the limits
+//Model dynamics are not taken into consideration, drive slow on curves moron!, a linear velocity scalation is proposed
+//note: Relationship between steeringwheel and direcction wheel rotation
